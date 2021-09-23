e03bb801c189299f8895fed789836747a5287a81
Popov <paracosm17@yandex.ru>
2021-09-23 14:18:12 +0300

    Homework

--- dnevnikru.py
+++ dnevnikru.py
@@ -11,9 +11,11 @@ class Defaults(enum.Enum):
     """
     dateFrom = date.today().strftime("%d.%m.%Y")
     dateTo = (date.today() + timedelta(days=10)).strftime("%d.%m.%Y")
-    studyYear = date.today().year
+    studyYear = date.today().strftime("%Y")
     choose = '%D0%9F%D0%BE%D0%BA%D0%B0%D0%B7%D0%B0%D1%82%D1%8C'
     group = 'all'
+    base_link = "https://schools.dnevnik.ru/"
+    hw_link = base_link + "homework.aspx?school={}&tab=&studyYear={}&subject=&datefrom={}&dateto={}&choose=" + choose
 
 
 class DnevnikError(Exception):
@@ -25,11 +27,15 @@ class DnevnikError(Exception):
 class Utils:
     @staticmethod
     def last_page(response):
-        soup = BeautifulSoup(response, 'lxml')
-        all_pages = soup.find('div', {'class': 'pager'})
-        pages = all_pages.find_all('li')
-        last_page = pages[-1].text
-        return last_page
+        try:
+            soup = BeautifulSoup(response, 'lxml')
+            all_pages = soup.find('div', {'class': 'pager'})
+            pages = all_pages.find_all('li')
+            last_page = pages[-1].text
+            return last_page
+        except DnevnikError:
+            last_page = None
+            return last_page
 
     @staticmethod
     def save_content(response, class2):
@@ -64,6 +70,44 @@ class Dnevnik:
             self.school = school
         except DnevnikError:
             raise DnevnikError('Invalid login data or wrong auth method', 'LoginError')
+
+    def homework(self, datefrom=Defaults.dateFrom.value, dateto=Defaults.dateTo.value, studyyear=Defaults.studyYear.value):
+        if len(datefrom) != 10 or len(dateto) != 10:
+            raise DnevnikError("Incorrect dateto or datefrom", "Parameters error")
+        if str(studyyear) not in datefrom or str(studyyear) not in dateto:
+            raise DnevnikError("StudyYear must be in dateto or in datefrom", "Parameters error")
+
+        link = Defaults.hw_link.value.format(self.school, studyyear, datefrom, dateto)
+        homework_response = self.main_session.get(link, headers={"Referer": link}).text
+        last_page = Utils.last_page(homework_response)
+
+        if last_page is not None:
+            subjects = []
+            for page in range(1, int(last_page) + 1):
+                homework_response = self.main_session.get(link+f"&page={page}", headers={"Referer": link}).text
+                for i in Utils.save_content(homework_response, class2='grid gridLines vam hmw'):
+                    subject = [i[2],
+                               i[0].replace('\n\r\n                        ', '').replace(
+                                   '\r\n                    \n', ''),
+                               i[3].replace('\n\n', '').replace('\xa0', ' ').replace('\r\n        \t\t\t ', '').replace(
+                                   '\r\n                \r\n\t\t\t\t    \n', '')]
+                    subjects.append(subject)
+            return subjects
+        if last_page is None:
+            try:
+                subjects = []
+                for i in Utils.save_content(homework_response, class2='grid gridLines vam hmw'):
+                    subject = [i[2],
+                               i[0].replace('\n\r\n                        ', '').replace(
+                                   '\r\n                    \n', ''),
+                               i[3].replace('\n\n', '').replace('\xa0', ' ').replace('\r\n        \t\t\t ', '').replace(
+                                   '\r\n                \r\n\t\t\t\t    \n', '')]
+                    subjects.append(subject)
+                return subjects
+            except DnevnikError:
+                return "Домашних заданий не найдено!"
+
+
 
 
 
